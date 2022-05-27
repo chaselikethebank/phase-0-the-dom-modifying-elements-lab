770ecae1c56666dd736fd5ad729f0b01e551b795
Chase <chasedemaster.tech@gmail.com>
2022-05-27 16:46:46 -0500

    commit tree 26ab3513f9eaf4e9ff1f2831e73d4b933fc36fbc

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const main = document.getElementById("main")
+const body = document.querySelector("body")
+body.removeChild(main);
+
+const newHeader = document.createElement("h1")
+newHeader.id = ("victory")
+newHeader.textContent = ("is the champion")
+document.body.append(newHeader)
+
+
+textContent = "YOUR-NAME is the champion";
